791927434bece71cf4ece98d12a97b5936d3e0ed
Mijail Rondon <mijail.rondon@gmail.com>
2018-03-26 03:14:16 -0400

    Redirect to root_path

--- app/controllers/witnesses_controller.rb
+++ app/controllers/witnesses_controller.rb
@@ -28,7 +28,7 @@ class WitnessesController < ApplicationController
 
     respond_to do |format|
       if @witness.save
-        format.html { redirect_to root, notice: 'Witness was successfully created.' }
+        format.html { redirect_to root_path, notice: 'Witness was successfully created.' }
       else
         format.html { render :new }
       end

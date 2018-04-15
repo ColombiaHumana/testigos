995d882f0f21f79768efc8fc8a220d55a45d77a7
Mijail Rondon <mijail.rondon@gmail.com>
2018-04-14 19:04:37 -0500

    rescue exceptions

--- app/models/witness.rb
+++ app/models/witness.rb
@@ -19,22 +19,25 @@ class Witness < ApplicationRecord
       versionnavegador: "5.1.1"
     }
     url = 'https://app.infovotantes.co/InfoVotantesWS/InfoServices/Servicios/consultarLugar'
-    request = RestClient.post(url, data.to_json, {content_type: :json, accept: :json})
-    json = JSON.parse(request)
-    if json["exito"]
-      if json["data"]["lugarVotacion"]
-        vote_post = json["data"]["lugarVotacion"]
-        self.departamento = vote_post["departamento"]
-        self.municipio = vote_post["municipio"]
-        self.puesto = vote_post["puesto"]
-        self.mesa = vote_post["mesa"]
-        self.zona = vote_post["codZona"]
-        true
-      elsif json["data"]["novedades"]
-        self.errors.add(:doc_number, json["data"]["novedades"]["nombreNovedad"])
-        false
+    begin
+      request = RestClient.post(url, data.to_json, {content_type: :json, accept: :json})
+      json = JSON.parse(request)
+      if json["exito"]
+        if json["data"]["lugarVotacion"]
+          vote_post = json["data"]["lugarVotacion"]
+          self.departamento = vote_post["departamento"]
+          self.municipio = vote_post["municipio"]
+          self.puesto = vote_post["puesto"]
+          self.mesa = vote_post["mesa"]
+          self.zona = vote_post["codZona"]
+          true
+        elsif json["data"]["novedades"]
+          self.errors.add(:doc_number, json["data"]["novedades"]["nombreNovedad"])
+          false
+        end
       end
-
+    rescue
+      false
     end
   end
 end
